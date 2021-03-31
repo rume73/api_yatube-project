ba4632be2d83637d736e0146af288e4ea22bb45d
Владимир <rume733@gmail.com>
2021-03-31 16:15:03 +0300

    first try

--- yatube_api/settings.py
+++ yatube_api/settings.py
@@ -92,9 +92,9 @@ REST_FRAMEWORK = {
         'api.permissions.IsOwnerOrReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-            'rest_framework_simplejwt.authentication.JWTAuthentication',
-        ],
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
-        ],
+    ],
 }
